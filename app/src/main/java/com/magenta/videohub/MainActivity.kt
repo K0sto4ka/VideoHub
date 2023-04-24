1b6b172fdfed970966ebfaed057220ad5a3e44a4
kosto4ka <krivcovkonstantin48@gmail.com>
2023-04-24 13:58:41 +0300

    commit

--- app/src/main/java/com/magenta/videohub/MainActivity.kt
+++ app/src/main/java/com/magenta/videohub/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.magenta.videohub
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.magenta.videohub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getPermision()
+    }
+
+    private fun getPermision() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA),101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermision()
+        }
     }
 }
